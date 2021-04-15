c08bab79e8930500be2727789db1326d7fc08f64
prosvetovvv <vp12@yandex.ru>
2021-04-15 17:49:06 +0300

    Add ResponseCodableTests

---
+++ GBShopTests/ResponseCodableTests.swift
@@ -0,0 +1,46 @@
+//
+//  ResponseCodableTests.swift
+//  GBShopTests
+//
+//  Created by Vitaly Prosvetov on 14.04.2021.
+//
+
+import XCTest
+import Alamofire
+
+@testable import GBShop
+
+class ResponseCodableTests: XCTestCase {
+    var errorParserStub: ErrorParserStub?
+    
+    override func setUpWithError() throws {
+        errorParserStub = ErrorParserStub()
+    }
+    
+    override func tearDownWithError() throws {
+        errorParserStub = nil
+    }
+    
+    func testShouldDownloadAndParse() throws {
+        //let download = XCTestExpectation(description: "Download https://failUrl")
+        let download = expectation(description: "Download https://failUrl")
+        let errorParser = try XCTUnwrap(errorParserStub)
+        
+        AF
+            .request("https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/login.json")
+            .responseCodable(errorParser: errorParser) { (response: AFDataResponse<PostStub>) in
+                switch response.result {
+                case .success(_):
+                    download.fulfill()
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                }
+            }
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    
+    
+    
+    
+}
